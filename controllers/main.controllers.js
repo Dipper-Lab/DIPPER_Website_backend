6a3d3dd47397206b81cf72f5339b5fafccfb9cba
king-aggor <109685452+king-aggor@users.noreply.github.com>
2024-04-29 16:21:57 +0000

    set up main controllers and routes

--- controllers/main.controllers.js
+++ controllers/main.controllers.js
@@ -3,3 +3,43 @@
 // local modules
 
 // third-party modules
+
+// get home page
+exports.getHomePage = (req, res, next) => {
+  res.status(200).json({ message: "HomePage" });
+};
+
+// get projects
+exports.getProjects = (req, res, next) => {
+  res.status(200).json({ message: "Projects" });
+};
+
+// get publications
+exports.getPublications = (req, res, next) => {
+  res.status(200).json({ message: "Publications" });
+};
+
+// get members
+exports.getMembers = (req, res, next) => {
+  res.status(200).json({ message: "Members" });
+};
+
+// get events
+exports.getEvents = (req, res, next) => {
+  res.status(200).json({ message: "Events" });
+};
+
+// get about
+exports.getAbout = (req, res, next) => {
+  res.status(200).json({ message: "About" });
+};
+
+// get contact us
+exports.getContactUs = (req, res, next) => {
+  res.status(200).json({ message: "ContactUs" });
+};
+
+// post contact us
+exports.postContactUs = (req, res, next) => {
+  res.status(200).json({ message: "ContactUs" });
+};
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,5 +1,37 @@
 // core modules
 
 // local modules
+const mainController = require("../controllers/main.controllers"); //importing main controller
 
 // third-party modules
+const express = require("express"); //importing express
+
+// define router
+const router = express.Router();
+
+// get home page
+router.get("/", mainController.getHomePage);
+
+// get projects
+router.get("/projects", mainController.getProjects);
+
+// get publications
+router.get("/publications", mainController.getPublications);
+
+// get members
+router.get("/members", mainController.getMembers);
+
+// get events
+router.get("/events", mainController.getEvents);
+
+// get about
+router.get("/about", mainController.getAbout);
+
+// get contact us
+router.get("/contact-us", mainController.getContactUs);
+
+// post contact us
+router.post("/contact-us", mainController.postContactUs);
+
+// export router
+module.exports = router;
