b2be3c3ff18d568beea1a898836050b51ffd6f68
king-aggor <109685452+king-aggor@users.noreply.github.com>
2024-06-14 16:19:45 +0000

    created authentication post login controller

--- controllers/authentication.controllers.js
+++ controllers/authentication.controllers.js
@@ -50,7 +50,52 @@ exports.postRegister = async (req, res, next) => {
 };
 
 //post login
-exports.postLogin = async (req, res, next) => {};
+exports.postLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    //check if email is of valid format
+    const emailFormat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/gi;
+    const match = email.match(emailFormat);
+    if (match == email) {
+      //find admin by email
+      const admin = await prisma.admin.findMany({
+        where: {
+          email: email,
+        },
+      });
+      //check if admin exists
+      if (admin.length > 0) {
+        //check if user password matches the hashed password in the database
+        const match = await bcrypt.compare(password, admin[0].password);
+        //if password matches, create token
+        if (match) {
+          const token = jwt.sign(
+            {
+              email: email,
+              password: admin[0].password,
+            },
+            process.env.jwt_key,
+            {
+              expiresIn: "1d",
+            }
+          );
+          res
+            .status(200)
+            .json({ message: "Admin logged in successfully", token });
+        } else {
+          res.status(422).json({ message: "Invalid password" });
+        }
+      } else {
+        res.status(422).json({ message: "Admin not found" });
+      }
+    } else {
+      res.status(422).json({ message: "Invalid email format" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(200).json({ message: err });
+  }
+};
 
 //post logout
 exports.postLogout = async (req, res, next) => {};
