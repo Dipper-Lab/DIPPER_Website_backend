894b75753cbe870bdb7053f5c61994c757d0406a
king-aggor <109685452+king-aggor@users.noreply.github.com>
2024-06-14 14:13:29 +0000

    created authentication post register controller

--- controllers/authentication.controllers.js
+++ controllers/authentication.controllers.js
@@ -1,11 +1,35 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client"); //importing prisma client
+const bcrypt = require("bcrypt"); //importing bcrypt
 
 const prisma = new PrismaClient();
 
 //post register
 exports.postRegister = async (req, res, next) => {
   try {
-    res.status(200).json({ message: "User created successfully" });
+    const { email, password } = req.body;
+    //check if there is an existing admin
+    const admin = await prisma.admin.findMany({});
+    if (admin.length > 0) {
+      res.status(422).json({ message: "There is an existing admin" });
+    } else {
+      // check if email is of valid format
+      const emailFormat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/gi;
+      const match = email.match(emailFormat);
+      if (match == email) {
+        // hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // create admin
+        const admin = await prisma.admin.create({
+          data: {
+            email: email,
+            password: hashedPassword,
+          },
+        });
+        res.status(200).json({ message: "Admin created successfully" });
+      } else {
+        res.status(422).json({ message: "Invalid email format" });
+      }
+    }
   } catch (err) {
     console.log(err);
     res.status(422).json({ message: err });
