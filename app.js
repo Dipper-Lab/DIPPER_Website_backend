093e90a99d25de536566817e19e5a6d3fc2fd5eb
king-aggor <109685452+king-aggor@users.noreply.github.com>
2024-06-19 13:34:58 +0000

    token in headers

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-    "token"
+    "Origin, X-Requested-With, Content-Type, Accept, token"
   );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow_Methods", "PUT, POST, PATCH, DELETE, GET");
